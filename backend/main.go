a53e072d79447c71d62677e8d04d614ad39f3369
MrMetel <metelartem22@gmail.com>
2025-10-15 15:42:10 +0300

    Edit in main.go 123

--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var response map[string]string
-
-	switch r.Method {
-	case http.MethodGet:
-		response = map[string]string{"message": "Hello, World!"}
-	case http.MethodPost:
-		response = map[string]string{"message": "You sent a POST request!"}
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		response = map[string]string{"error": "Method not allowed"}
-	}
-	json.NewEncoder(w).Encode(response)
+	fmt.Fprintln(w, "Hello, World!")
 }
 
 func main() {
