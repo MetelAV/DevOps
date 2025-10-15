aeac46b7ae51ada2e499f43507ccad65e6d34199
MrMetel <metelartem22@gmail.com>
2025-10-15 15:47:44 +0300

    Edit in main.go 12345

--- backend/main.go
+++ backend/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8081", nil)
+	fmt.Print("q")
 }
